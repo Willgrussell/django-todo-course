8574e956ae1532c1cf45208cb994676b6d414d9c
William Russell <russellgwill@gmail.com>
2018-07-15 11:35:40 +0000

    addition of .py files and database connection from heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse("postgres://kerdggromopbnx:5e5138e2b236ef8bbea23b66a56eb7bb976b5d6be907a1574c75436e4d41c6ec@ec2-54-217-205-90.eu-west-1.compute.amazonaws.com:5432/d4sb1599jufoa1")}
 
 
 # Password validation
